e6d1fdca2e5bf0aa382fa701d41e0427075282ab
Luke K <lak16@students.uwf.edu>
2016-09-07 11:01:58 -0500

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -17,8 +17,10 @@ int main(int argc, char** argv) {
 
     char command[500];
     const char delimiters[] = " \t\n";
+    
     Param_t *inputCommand;
     inputCommand = (Param_t *)malloc(sizeof(Param_t));
+    inputCommand->argumentCount = 0;
     
     printf("Enter a command with no more than 32 arguments.\n");
     fgets(command, 500, stdin);
@@ -30,6 +32,7 @@ int main(int argc, char** argv) {
         tokenize(command, delimiters, inputCommand);
     }
     
+    printParams(inputCommand);
+    
     return 0;
 }
-

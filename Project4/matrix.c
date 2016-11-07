ccfa31ee86ef00a2aadf5427de156a4f64373cab
ArgoFromLargo <lukekledzik@gmail.com>
2016-11-06 19:09:05 -0600

    Updated matrix.c

--- Project4/matrix.c
+++ Project4/matrix.c
@@ -1,10 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char matrix[20480][4096]; // GLOBAL ARRAY
+
 int main(int argc, char** argv) {
 
-    char matrix[20480][4096];
+
 
     return 0;
 
+}
+
+void writeRow() {
+    int i, j;
+    for(i = 0; i < 20480; i++) {
+        for(j = 0; j < 4096; j++) {
+            matrix[i][j] = 'R';
+        }
+    }
+}
+
+void writeColumn() {
+    int i, j;
+    for(i = 0; i < 4096; i++) {
+        for(j = 0; j < 20480; j++) {
+            matrix[i][j] = 'C';
+        }
+    }
+}
+
+void readRow() {
+    char readVal;
+    int i, j;
+    for(i = 0; i < 20480; i++) {
+        for(j = 0; j < 4096; j++) {
+            readVal = matrix[i][j];
+        }
+    }
+}
+
+void readColumn() {
+    char readVal;
+    int i, j;
+    for(i = 0; i < 4096; i++) {
+        for(j = 0; j < 20480; j++) {
+            readVal = matrix[i][j];
+        }
+    }
 }

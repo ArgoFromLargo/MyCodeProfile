4e2d5559629b8eec09280664d78f8bc39acb8cc4
ArgoFromLargo <lukekledzik@gmail.com>
2016-11-06 19:46:22 -0600

    Updated matrix.c. Fully operational.

--- Project4/matrix.c
+++ Project4/matrix.c
@@ -1,15 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+void writeRow();
+void writeColumn();
+void readRow();
+void readColumn();
 
 char matrix[20480][4096]; // GLOBAL ARRAY
 
 int main(int argc, char** argv) {
 
+    int i, secs, milliSecs;
+    clock_t start, finish, total;
 
+    // WRITE BY ROW
+
+    start = clock();
+    for(i = 0; i < 10; i++) {
+        writeRow();
+    }
+    finish = clock();
+
+    total = (finish - start) / 10; // AVERAGE THE TIMES
+    milliSecs = total * 1000 / CLOCKS_PER_SEC;
+    secs = milliSecs / 1000;
+    milliSecs = milliSecs % 1000;
+
+    printf("Time to fill array row-by-row: %d.%d\n", secs, milliSecs);
+
+    // WRITE BY COLUMN
+
+    start = clock();
+    for(i = 0; i < 10; i++) {
+        writeColumn();
+    }
+    finish = clock();
+
+    total = (finish - start) / 10; // AVERAGE THE TIMES
+    milliSecs = total * 1000 / CLOCKS_PER_SEC;
+    secs = milliSecs / 1000;
+    milliSecs = milliSecs % 1000;
+
+    printf("Time to fill array column-by-column: %d.%d\n", secs, milliSecs);
+
+    // READ BY ROW
+
+    start = clock();
+    for(i = 0; i < 10; i++) {
+        readRow();
+    }
+    finish = clock();
+
+    total = (finish - start) / 10; // AVERAGE THE TIMES
+    milliSecs = total * 1000 / CLOCKS_PER_SEC;
+    secs = milliSecs / 1000;
+    milliSecs = milliSecs % 1000;
+
+    printf("Time to read array row-by-row: %d.%d\n", secs, milliSecs);
+
+    // READ BY COLUMN
+
+    start = clock();
+    for(i = 0; i < 10; i++) {
+        readColumn();
+    }
+    finish = clock();
+
+    total = (finish - start) / 10; // AVERAGE THE TIMES
+    milliSecs = total * 1000 / CLOCKS_PER_SEC;
+    secs = milliSecs / 1000;
+    milliSecs = milliSecs % 1000;
+
+    printf("Time to read array column-by-column: %d.%d\n", secs, milliSecs);
 
     return 0;
 
 }
+
+// READING AND WRITING FUNCTIONS
 
 void writeRow() {
     int i, j;
